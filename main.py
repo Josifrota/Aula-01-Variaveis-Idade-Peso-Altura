d55ff1f14fe3851c6071a272e15ffe45c61f17fc
josi27frota <27606125-josi27frota@users.noreply.replit.com>
2023-11-13 17:31:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# declaração de variáveis 
+nome = "Josiane"
+idade = 47
+altura = 1.60
+peso = 67.0
+
+
+
+print("Olá, mundo!")
+
+#imprimir na tela o valor das variáveis
+print(nome)
+print(idade)
+print(altura)
+print(peso)
+
+print("O meu nome é", nome, ", tenho", idade, "anos e peso ", peso, "quilos.")
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(peso))
+
+print(2 * '\n')
+print("-------------------------")
+print(2 * '\n')
+
+nome1 = input("Digite o seu nome: ")
+idade1 = int(input("Digite a sua idade: "))
+altura1 = float(input("Digite a sua altura: "))
+print("Seu nome é", nome1, ", você tem", idade1, "anos e", altura1, "de altura.")
+
+
